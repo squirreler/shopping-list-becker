a2635ab2352df10962b08b350fc0e9f4df85ea10
Daniel B <dbbemailme@gmail.com>
2024-08-17 23:26:44 -0700

    starting to work on javascript

---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+<li
+    class="flex flex-row justify-between items-center min-w-[200px] bg-emerald-300 rounded-md mx-[20vw] px-5 py-6 text-emerald-950 text-3xl"
+>
+    <p class="justify-center">Hello</p>
+    <svg
+        class="flex shrink-0 h-8 w-8 text-red-500"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    >
+        <path stroke="none" d="M0 0h24v24H0z" />
+        <line x1="18" y1="6" x2="6" y2="18" />
+        <line x1="6" y1="6" x2="18" y2="18" />
+    </svg>
+</li>
+
+*/
+
+let submitButton = document.getElementById("submit-button");
+let listNameInputBox = document.getElementById("list-name-input");
+let itemList = document.getElementById("item-list");
+console.log(listNameInputBox);
