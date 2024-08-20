b76ba33ba84efb1da2fe8d6c1483e1d0383a8e26
Daniel B <dbbemailme@gmail.com>
2024-08-19 18:54:28 -0700

    after much suffering, I have finished the most basic features. There is so much more I could do, but I need to get this over with

--- index.js
+++ index.js
@@ -2,47 +2,22 @@ const listItemTailwindClassesString =
   "flex flex-row justify-between items-center min-w-[200px] bg-emerald-300 rounded-md mx-[20vw] px-5 py-6 text-emerald-950 text-3xl";
 const listItemClassList = listItemTailwindClassesString.split(" ");
 
-/*
-<li
-    class="flex flex-row justify-between items-center min-w-[200px] bg-emerald-300 rounded-md mx-[20vw] px-5 py-6 text-emerald-950 text-3xl"
->
-    <p class="justify-center">Hello</p>
-    <svg
-        class="flex shrink-0 h-8 w-8 text-red-500"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        stroke-width="2"
-        stroke="currentColor"
-        fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-    >
-        <path stroke="none" d="M0 0h24v24H0z" />
-        <line x1="18" y1="6" x2="6" y2="18" />
-        <line x1="6" y1="6" x2="18" y2="18" />
-    </svg>
-</li>
-
-*/
-
 let submitButton = document.getElementById("submit-button");
 let listNameInputBox = document.getElementById("list-name-input");
 let itemList = document.getElementById("item-list");
-
+let clearButton = document.getElementById("clear-button");
 // console.log(listNameInputBox);
 // console.log(itemList.children);
 // console.log(itemList.children[0]);
-function createSvg() {
-  svg = document.createElement("svg");
-  svg.innerHTML = `<path stroke="none" d="M0 0h24v24H0z" />`;
-  svg.innerHTML += `<line x1="18" y1="6" x2="6" y2="18" />`;
-  svg.innerHTML += `<line x1="6" y1="6" x2="18" y2="18" />`;
-  svg.classList.add("flex", "shrink-0", "h-8", "w-8", "text-red-500");
-  svg.setAttribute("width", "24");
-  svg.setAttribute("height", "24");
-  //Fill in the rest
-  return svg;
+function createXIcon() {
+  //Could not find out how to dynamically render svgs, so this will have to do
+  let icon = document.createElement("button");
+  icon.classList.add("text-red-500");
+  icon.classList.add("font:bold");
+  iconCharacter = document.createTextNode("x");
+  icon.appendChild(iconCharacter);
+  icon.classList.add("flex", "justify-center", "content-center");
+  return icon;
 }
 function createAndAddListItemToListInDom(listItemName) {
   console.log("The name of the soon to be list item is: " + listItemName);
@@ -55,7 +30,7 @@ function createAndAddListItemToListInDom(listItemName) {
   listItemNameParagraph.appendChild(document.createTextNode(listItemName));
   listItemNameParagraph.classList.add("justify-center");
 
-  let svgIcon = createSvg();
+  let svgIcon = createXIcon();
 
   newListItem.appendChild(listItemNameParagraph);
   newListItem.appendChild(svgIcon);
@@ -64,10 +39,6 @@ function createAndAddListItemToListInDom(listItemName) {
 function removeListItemFromDom(listItem) {
   listItem.remove();
 }
-function removeListItemFromLocalStorage(listItem) {}
-function addListItemToLocalStorage() {}
-function clearListItemsInLocalStorage() {}
-function addListItemsInLocalStorageToDOM() {}
 
 function getParentListItem(clickedElement) {
   clickedItemParent = clickedElement.parentElement;
@@ -94,12 +65,15 @@ function handleListModification(event) {
   clickedElementName = clickedElement.tagName;
   console.log(clickedElementName);
   console.log(getParentListItem(clickedElement));
-  if (clickedElementName == "svg" || clickedElementName == "line") {
+  if (clickedElementName == "BUTTON") {
     removeListItemFromDom(getParentListItem(clickedElement));
     //removeListItemFromLocalStorage
   }
 }
 function getListItemSubmissionNameFromInput(submittedListItemName) {
+  if (submittedListItemName === " " || submittedListItemName === "") {
+    return;
+  }
   //Format the name
   submittedListItemName =
     submittedListItemName[0].toUpperCase() +
@@ -137,14 +111,23 @@ function handleNewListItemSubmission() {
     listNameInputBox.value,
   );
   if (newListItemName == null) {
-    alert("The name is a duplicate");
+    alert("The name is a duplicate or invalid");
     return;
   }
   console.log("The name: " + newListItemName);
   createAndAddListItemToListInDom(newListItemName);
+  listNameInputBox.value = "";
+  console.log(clearButton.display);
+  console.log(itemList.firstChild);
+}
+function handleClear() {
+  while (itemList.firstChild != null) {
+    removeListItemFromDom(itemList.firstChild);
+  }
 }
 itemList.addEventListener("click", handleListModification);
 submitButton.addEventListener("click", handleNewListItemSubmission);
+clearButton.addEventListener("click", handleClear);
 //Find out how to create li
 //Find out how to insert li into nodelist
 //Find out how to delete li from nodelist
